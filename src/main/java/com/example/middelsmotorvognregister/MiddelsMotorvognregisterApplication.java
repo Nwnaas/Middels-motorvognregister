7f7e21aebd95df8f27fdeda21c88cd4ef5902f77
“Nwnaas” <nwnaas@icloud.com>
2024-01-04 13:08:17 +0100

    Initial commit

---
+++ src/main/java/com/example/middelsmotorvognregister/MiddelsMotorvognregisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.middelsmotorvognregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddelsMotorvognregisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddelsMotorvognregisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/middelsmotorvognregister/MiddelsMotorvognregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.middelsmotorvognregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddelsMotorvognregisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
